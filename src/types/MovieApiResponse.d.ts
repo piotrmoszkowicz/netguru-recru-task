c91abc23f9813fe1c5d785e797deab5a3c102d19
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-02-20 03:33:36 +0100

    Add typings needed for Movie

---
+++ src/types/MovieApiResponse.d.ts
@@ -0,0 +1,34 @@
+interface RatingApi {
+  Source: string;
+  Value: string;
+}
+
+interface MovieApiResponse {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: RatingApi[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response?: string;
+}
+
+export default MovieApiResponse;
